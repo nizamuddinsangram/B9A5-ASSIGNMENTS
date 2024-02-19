339a860479697b41a70084ed30a31643e7b9d3e4
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-02-19 13:28:08 +0600

    add

--- ApplyJs.js
+++ ApplyJs.js
@@ -5,10 +5,28 @@ for (const seat of seats) {
   seat.addEventListener("click", function (event) {
     // console.log("asi ra bai asi");
     count = count + 1;
-    console.log(
+    const ticketPrice =
       event.target.parentNode.parentNode.parentNode.parentNode.childNodes[5]
-        .childNodes[3].childNodes[3].childNodes[0].innerText
-    );
+        .childNodes[3].childNodes[3].childNodes[0].innerText;
+    const ticketCategory = event.target.innerText;
+    // console.log(
+    //   event.target.parentNode.parentNode.parentNode.parentNode.childNodes[5]
+    //     .childNodes[3].childNodes[3].childNodes[0].innerText
+    // );
+    // console.log(event.target.innerText);
+    const buySeat = document.getElementById("append");
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerText = ticketCategory;
+    // const p2 = document.createElement("p");
+    // p2.innerHTML = "Economy";
+    const p3 = document.createElement("p");
+    p3.innerText = ticketPrice;
+    li.appendChild(p);
+    // buySeat.appendChild(p2);
+    li.appendChild(p3);
+    buySeat.appendChild(li);
+
     setInnerText("bookings-seats", count);
   });
 }
